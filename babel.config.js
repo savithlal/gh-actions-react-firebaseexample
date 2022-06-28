bdc667ddafae3119e41bcaec51f817db6ffc3382
savithlal <savith.lal@bytegate.com>
2022-06-28 16:25:54 +0530

    changes

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    // Reanimated plugin has to be listed last.
-    'react-native-reanimated/plugin',
-],
-};
